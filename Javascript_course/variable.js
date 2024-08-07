2e5df74e3b39b15b500e84f3194e42392d1a388f
Shehroz Ali <shehrozrajout829@gmail.com>
2024-08-07 19:50:05 +0000

    container config

---
+++ Javascript_course/variable.js
@@ -0,0 +1,13 @@
+const accountId = 132;
+let accountEmail = "bscsf21a126@gmail.com";
+var accountPassword = "scts9200";
+accountCity = "Jhelum";
+var accountState;
+// console.log(accountId);
+// accountId = 126; // not allowed aver-write in constant 
+
+accountEmail = "shehrozrajout829@gmail.com";
+accountPassword = "victory8588";
+accountCity = "Lahore";
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
