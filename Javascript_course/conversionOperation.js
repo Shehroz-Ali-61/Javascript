59e7cb00b7e4e1c096da8516967305113cb9d31a
Shehroz Ali <shehrozrajout829@gmail.com>
2024-08-08 18:50:35 +0000

    container config

---
+++ Javascript_course/conversionOperation.js
@@ -0,0 +1,11 @@
+let score = "scehroz";
+
+// let score = null;
+// let score = undefined;
+// let score = true;
+// console.log(typeof score);
+
+
+let isNumber = Number(score);
+console.log(typeof(isNumber));
+console.log(isNumber);
