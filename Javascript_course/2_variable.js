3df08a9d946fc53d4885e721d3c3fcd172eb0f3f
Shehroz Ali <shehrozrajout829@gmail.com>
2024-08-11 15:17:07 +0000

    variable details

---
+++ Javascript_course/2_variable.js
@@ -0,0 +1,18 @@
+const accountId = 132;
+let accountEmail = "bscsf21a126@gmail.com";
+var accountPassword = "scts9200";
+accountCity = "Jhelum"; 
+var accountState;
+// console.log(accountId);
+// accountId = 126; // not allowed aver-write in constant 
+
+accountEmail = "shehrozrajout829@gmail.com";
+accountPassword = "victory8588";
+accountCity = "Lahore"
+
+/*  Note:
+    prefer not to use var
+    because on issue in block space and functional scope
+*/
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
