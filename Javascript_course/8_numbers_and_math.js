ce8f4b0a0f9181f664474e3c856d1cff2b970148
Shehroz Ali <shehrozrajout829@gmail.com>
2024-08-11 15:28:33 +0000

    Number Objects and Math Objects

---
+++ Javascript_course/8_numbers_and_math.js
@@ -0,0 +1,44 @@
+//_________________________ Numbers ____________________________
+
+const score = 44; //Note: it's also a Numbr type and show anly value.
+// console.log(score);
+
+const balance = new Number(100); //Note: it's also a Numbr type but show "value with Type".
+// console.log(balance);
+
+// console.log(balance.toString()); //Returns a value in string representation of an object.
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2)); //Returns a string representing a number in fixed-point notation.
+
+let otherNumber = 126.26;
+// console.log(otherNumber.toPrecision(3));
+let pointNumber = 499.66;
+// console.log(pointNumber.toPrecision(3));
+
+const hundered = 10000000;
+// console.log(hundered.toLocaleString());
+// console.log(hundered.toLocaleString("en-IN")); // Acc to indian currency
+// console.log(Number.MAX_VALUE);
+
+
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+//___________________________ Math _____________________________
+// console.log(Math);
+// console.log(Math.abs(-23));
+// console.log(Math.round(69.5));
+// console.log(Math.ceil(36.65)); // Return Uper value
+// console.log(Math.floor(103.3)); // Return Lower value
+// console.log(Math.pow(2,4));
+// console.log(Math.random());
+
+
+// console.log(Math.random());
+// console.log(Math.random() * 10);
+// console.log((Math.random() * 10) + 1);
+// console.log(Math.floor((Math.random() * 10) + 1));
+
+let min = 10;
+let max = 20;
+console.log(Math.random() * (max - min + 1));
+console.log(Math.random() * (max - min + 1) + min);
+console.log(Math.floor(Math.random() * (max - min + 1) + min));
