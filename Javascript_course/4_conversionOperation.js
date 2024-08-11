406bc42cd51d3244a9b461a53997797b8eb70198
Shehroz Ali <shehrozrajout829@gmail.com>
2024-08-11 15:25:26 +0000

    Arithmatic, Logical, Comparison operations

---
+++ Javascript_course/4_conversionOperation.js
@@ -0,0 +1,39 @@
+let score = "shehroz";
+
+// let score = null;
+// let score = undefined;
+// let score = true;
+// console.log(typeof score);
+
+
+let isNumber = Number(score);
+// console.log(typeof(isNumber));
+// console.log(isNumber);
+
+let isbolean = 0;
+let boleanislogedin = Boolean(isbolean);
+// console.log(boleanislogedin);
+        //Boolean Details
+// 1 => true;  0 => false
+// "" => false
+// "Pakistan" => true
+
+let isString = 33;
+let stringNumber = String(isString);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+// conditional operators
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+
+// clean code "===" in this operator we check both value and it's data type.
+console.log("2"===2);  // => false
+ 
+
+
+
+
