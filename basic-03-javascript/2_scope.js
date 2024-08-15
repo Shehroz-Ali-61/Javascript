8ed24eb660bd985fc78a2dcbc662f729d1ce9420
Shehroz Ali <shehrozrajout829@gmail.com>
2024-08-15 18:37:19 +0000

    scope of let, const, var

--- basic-03-javascript/2_scope.js
+++ basic-03-javascript/2_scope.js
@@ -1,4 +1,17 @@
-let a = 10;
+/*let a = 10;
 const b = 20;
+var c = 30;*/
+
+let a = 200;
 var c = 30;
-console.log(`${a} , ${b} , ${c}`);
+if(true){
+    let a = 10;
+    const b = 20;
+    var c = 65;
+    console.log("a:",a);
+
+}
+console.log(a);
+console.log(c);
+
+// console.log(`${a} , ${b} , ${c}`);
