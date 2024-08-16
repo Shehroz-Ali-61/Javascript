6086a063112fc9188e66144e481a7fa355e3b7fc
Shehroz Ali <shehrozrajout829@gmail.com>
2024-08-16 15:01:52 +0000

    Scope of Let, var, const in function and conditional statmnt + Declaration

--- basic-03-javascript/2_scope.js
+++ basic-03-javascript/2_scope.js
@@ -1,7 +1,7 @@
 /*let a = 10;
 const b = 20;
 var c = 30;*/
-
+/*
 let a = 200;
 var c = 30;
 if(true){
@@ -14,4 +14,57 @@ if(true){
 console.log(a);
 console.log(c);
 
-// console.log(`${a} , ${b} , ${c}`);
+// console.log(`${a} , ${b} , ${c}`);   */
+
+
+
+
+
+
+
+//++++++++++++++++ Scope of the Function +++++++++++++
+function user(){
+    const username = "Shehroz";
+    function id(){
+        const rollno = 126;
+        console.log(username);
+    }
+    // console.log(rollno);
+    id();
+}
+// console.log(username);
+user();
+
+
+
+
+
+
+
+//++++++++++++++++ Scope of Conditional statement +++++++++++++
+if(true){
+    const name = "SYAZ";
+    if(name === "SYAZ"){
+        const password = "sayaz829";
+        console.log(name);
+    }
+    // console.log(password);
+}
+// console.log(name);
+
+
+
+
+//++++++++++++++++ Many way of Function Declaration ++++++++++++++++++
+console.log(addone(5));
+function addone(number){
+    return number + 1;
+}
+// addone(5);
+
+
+console.log(addtwo(7)); // <== There are error. Cannot access 'addtwo' before initialization
+const addtwo = function(num){
+    return num + 2;
+}
+// console.log(addtwo(7));
