f6d09f7e4b7e367742f5009dbe92c9745549a432
Shehroz Ali <shehrozrajout829@gmail.com>
2024-08-22 14:42:10 +0000

    ?? operator for choicion the function and compare with Terniary Operator

--- control-flow-04-javascript/3_truthy.js
+++ control-flow-04-javascript/3_truthy.js
@@ -13,8 +13,34 @@ function show(num1,num2){
 function mul(){
     return 4+5;
 }
-
 console.log(value);
+
+
+//false values: false, 0 , -0, "", null, undefined, NaN
+// truely values: "0", 'false', " ", [], {}, function(){}
+
+
+let email = "shehrozali@gmail.com";
+if(email.length==0){
+    console.log("plz enter email");
+}
+else{
+    console.log(`email: ${email}`);
+}
+
+
+//------- Checking empty Object and Array -------
+const emptyobj = {};
+if(Object.keys(emptyobj).length === 0){
+    console.log("Empty Object");
+}
+
+const emptyarry = [];
+if(emptyarry.length === 0){
+    console.log("Empty Array");
+}
+
+
 
 
 
