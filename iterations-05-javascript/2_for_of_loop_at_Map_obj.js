dcd64c6a0a2eeb9503e86491a7811623ba5c3d53
Shehroz Ali <shehrozrajout829@gmail.com>
2024-08-22 18:19:04 +0000

    use for of loop and at Map Object

--- iterations-05-javascript/2_for_of_loop_at_Map_obj.js
+++ iterations-05-javascript/2_for_of_loop_at_Map_obj.js
@@ -4,7 +4,7 @@ for(let i of array){
     //console.log(i);
 }
 
- 
+//"NOTE": /"NOTE": for-of loop ky andr "key" or "value" dono sy he key or value access ki ja skti hai. yih zada tar Map() Object main Use hota hai
 
 //------for of loop at Map Object -------
 const map = new Map();
