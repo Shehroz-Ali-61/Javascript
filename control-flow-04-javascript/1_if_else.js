eac64a785f94394cedb0c5540c30a1ff4fda848b
Shehroz Ali <shehrozrajout829@gmail.com>
2024-08-22 07:31:16 +0000

    If else condition

---
+++ control-flow-04-javascript/1_if_else.js
@@ -0,0 +1,22 @@
+/*
+const userloggedin = true;
+if(userloggedin){
+    let user = "Account LoggedIn";
+    console.log(`Your user ${user}`)
+}
+*/
+
+
+
+
+
+const balance = 0;
+if(balance < 20){
+    console.log("Low Balance");
+}
+else if(balance < 50){
+    console.log(`your balance is ${balance}`);
+}
+else{
+    console.log(`your balance is ${balance}`);
+}
