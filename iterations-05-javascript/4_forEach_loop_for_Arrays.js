cbaf87718efce3b73f05cc8e643c7555ca3ba2c6
Shehroz Ali <shehrozrajout829@gmail.com>
2024-08-23 16:37:35 +0000

    forEach loop in Array & object in Array

--- iterations-05-javascript/4_forEach_loop_for_Arrays.js
+++ iterations-05-javascript/4_forEach_loop_for_Arrays.js
@@ -18,7 +18,31 @@ function printme(item){
 
 
 
-
 coding.forEach( (item, index, arr)=>{   //---- 4
-    console.log(item, index, arr)
+    //console.log(item, index, arr)
 } )
+
+
+
+
+//--------- [{},{},{}]  => Object in Array => this is used in forEach loop ------------
+const myCoding = [
+    {
+        mobileName: "Iphone",
+        price: "3Lakh"
+    },
+    {
+        mobileName: "Samsung A71",
+        price: "81K"
+    },
+    {
+        mobileName: "Infinik Hot10",
+        price: "50K"
+    },
+    
+];
+myCoding.forEach((item)=>{
+    // console.log(item);
+    // console.log(item.mobileName);
+    console.log(`${item.mobileName}:- ${item.price}`);
+});
