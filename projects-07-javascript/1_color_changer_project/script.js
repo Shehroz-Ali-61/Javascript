24f7d43d47ea4a28c7d37b89dc51f9759b0a3ce0
Shehroz Ali <shehrozrajout829@gmail.com>
2024-10-20 16:35:26 +0000

    mention here that how to change color of body by javascript

---
+++ projects-07-javascript/1_color_changer_project/script.js
@@ -0,0 +1,24 @@
+const buttons = document.querySelectorAll('.button');
+const body = document.querySelector('body');
+
+buttons.forEach(function(button){
+    button.addEventListener('click',function(e){
+        // console.log(e);
+        // console.log(e.target);
+        if(e.target.id==='grey'){
+            body.style.backgroundColor= e.target.id;
+        }
+        if(e.target.id==='red'){
+            body.style.backgroundColor= e.target.id;
+        }
+        if(e.target.id==='green'){
+            body.style.backgroundColor= e.target.id;
+        }
+        if(e.target.id==='blue'){
+            body.style.backgroundColor= e.target.id;
+        }
+        
+    })
+})
+
+
